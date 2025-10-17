66988dc952a014f92c07c2987f4bd89d1d0ca8e8
eliseovillaok <eliseovilla10@gmail.com>
2025-10-16 21:11:23 -0300

    Add Autores and Libros modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LibrosModule } from './libros/libros.module';
+import { AutoresModule } from './autores/autores.module';
 
 @Module({
-  imports: [],
+  imports: [LibrosModule, AutoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
