e6de9b6c523315e3800b8deeb7a52c2419680560
eliseovillaok <eliseovilla10@gmail.com>
2025-10-16 21:25:41 -0300

    Add relationships and properties to Autore and Libro entities

--- src/autores/entities/autore.entity.ts
+++ src/autores/entities/autore.entity.ts
@@ -1,5 +1,15 @@
+import { Libro } from "src/libros/entities/libro.entity";
+import { Column, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
 export class Autore {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     nombre: string;
+    @Column()
     nacionalidad: string;
+    @OneToMany(() => Libro, (libro) => libro.autor) 
+    libros: Libro[];
 }
--- src/libros/entities/libro.entity.ts
+++ src/libros/entities/libro.entity.ts
@@ -1,8 +1,13 @@
 import { Autore } from "src/autores/entities/autore.entity";
-
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
 export class Libro {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     titulo: string;
+    @Column()
     genero: string;
+    @ManyToOne(() => Autore, {eager: true})
     autor: Autore;
 }
